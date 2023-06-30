882cd54abb29b1ef259138c10b41aafdcf691d1d
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-06-30 19:18:56 -0300

    adição da tela add-itens

--- src/app/modules/dashboard/cardapio/cardapio.module.ts
+++ src/app/modules/dashboard/cardapio/cardapio.module.ts
@@ -5,13 +5,15 @@ import { CardapioRoutingModule } from './cardapio-routing.module';
 import { TabLayoutComponent } from './tab-layout/tab-layout.component';
 import { TabCategoriasComponent } from './tab-categorias/tab-categorias.component';
 import { TabItensComponent } from './tab-itens/tab-itens.component';
+import { AddItensComponent } from './tab-itens/add-itens/add-itens.component';
 
 
 @NgModule({
   declarations: [
     TabLayoutComponent,
     TabCategoriasComponent,
-    TabItensComponent
+    TabItensComponent,
+    AddItensComponent
   ],
   exports: [
     TabLayoutComponent,
