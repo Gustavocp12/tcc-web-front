a7ff8bd30a88c2974ad7c3e48b945404806eaed3
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-06-29 21:29:59 -0300

    tab itens

--- src/app/modules/dashboard/cardapio/tab-itens/tab-itens.component.ts
+++ src/app/modules/dashboard/cardapio/tab-itens/tab-itens.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tab-itens',
   templateUrl: './tab-itens.component.html',
   styleUrls: ['./tab-itens.component.scss']
 })
-export class TabItensComponent {
+export class TabItensComponent implements OnInit {
+
+  tableValue = '';
+  empty = false;
+
+  tableOption = [
+    { title: 'Foto' },
+    { title: 'Nome' },
+    { title: 'Descrição' },
+    { title: 'Preço' },
+    { title: 'Disponível' },
+    { title: 'Estoque' }
+  ];
+
+  emptyTable(){
+    this.empty = this.tableValue == '';
+  }
+
+  ngOnInit() {
+    this.emptyTable();
+    console.log(this.empty);
+  }
 
 }
