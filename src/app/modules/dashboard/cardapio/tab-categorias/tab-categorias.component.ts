3f61b444bc40965925b3a498f88424840957bebf
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-06-29 21:29:28 -0300

    tab categorias

--- src/app/modules/dashboard/cardapio/tab-categorias/tab-categorias.component.ts
+++ src/app/modules/dashboard/cardapio/tab-categorias/tab-categorias.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tab-categorias',
   templateUrl: './tab-categorias.component.html',
   styleUrls: ['./tab-categorias.component.scss']
 })
-export class TabCategoriasComponent {
+export class TabCategoriasComponent implements OnInit {
+
+  available = true;
+  textAvailable = 'Sim';
+  empty = false;
+  tableValue = '';
+
+  tableOption = [
+    { title: 'Foto' },
+    { title: 'Nome' },
+    { title: 'Descrição' },
+    { title: 'Preço' },
+    { title: 'Disponível' },
+    { title: 'Estoque' }
+  ];
+
+  switchAvailable(){
+    this.available = !this.available;
+    this.textAvailable = this.available ? 'Sim' : 'Não';
+  }
+
+  emptyTable(){
+    this.empty = this.tableValue == '';
+  }
+
+  ngOnInit() {
+    this.emptyTable();
+    console.log(this.empty);
+  }
 
 }
